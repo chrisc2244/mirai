be3776f0363f88168e506da4c9a3bed681a8f055
Jonathan Cavaliere <98484493+Broke-Code@users.noreply.github.com>
2023-01-12 00:16:23 -0500

    Update Matrix.h
    Merge conflict resolution and formatting changes

--- src/Matrix.h
+++ src/Matrix.h
@@ -39,6 +39,27 @@ public:
 
 	~Matrix();
 
+	/**
+	* Places a submatrix of the CALLING object into the destination matrix, note that destination matrix
+	* must have a width and height in columns and rows for this function to work.
+	*
+	* @param startcol - The column (x index) of the first element to put in the submatrix
+	* @param startrow - The row (y index) of the first element to put in the submatrix
+	* @param destination - A reference to an intialized matrix in which the put the submatrix
+	*
+	* @throws out_of_bounds exceptions if startcol, startrow, or that index + destination columns and rows is out of bounds.
+	*/
+	void putSubMatrix(uint16_t startcol, uint16_t startrow, Matrix& destination);
+	
+	// Returns number of rows (height)
+	inline uint16_t getRowAmount() const { return m_rows; }
+
+	// Returns number of columns (width)
+	inline uint16_t getColumnAmount() const { return m_columns;  }
+
+	// Returns the size of the matrix in elements (columns * rows)
+	inline uint16_t getSize() const { return m_size; }
+	
 	//multiplies two matrices together index by index
 	//ex: (matrix[1] * matrix[1]) then divide by the total sum of pixels (sum / m_size)
 	//essentially the result of applying the filter 
@@ -55,27 +76,6 @@ public:
 	//ignore this, I want it later
 	Matrix getSubMatrix(uint16_t start_row, uint16_t start_col, uint16_t window_rows, uint16_t window_cols);
 
-	/**
-	* Places a submatrix of the CALLING object into the destination matrix, note that destination matrix
-	* must have a width and height in columns and rows for this function to work.
-	*
-	* @param startcol - The column (x index) of the first element to put in the submatrix
-	* @param startrow - The row (y index) of the first element to put in the submatrix
-	* @param destination - A reference to an intialized matrix in which the put the submatrix
-	*
-	* @throws out_of_bounds exceptions if startcol, startrow, or that index + destination columns and rows is out of bounds.
-	*/
-	void putSubMatrix(uint16_t startcol, uint16_t startrow, Matrix& destination);
-
-	// Returns number of rows (height)
-	inline uint16_t getRowAmount() const { return m_rows; }
-
-	// Returns number of columns (width)
-	inline uint16_t getColumnAmount() const { return m_columns;  }
-
-	// Returns the size of the matrix in elements (columns * rows)
-	inline uint16_t getSize() const { return m_size; }
-
 	//prints values in Matrix		[0.4213][0.4213][0.4213]
 	//3x3 example, 6 values total	[0.4213][0.4213][0.4213]
 	static void print(const Matrix& MatrixToPrint);
@@ -90,9 +90,9 @@ public:
 	//operators
 	Matrix operator+(const Matrix& otherMatrix) const;
 	Matrix operator-(const Matrix& otherMatrix) const;
-	Matrix operator*(const Matrix& otherMatrix) const;				// Dot Product
+	Matrix operator*(const Matrix& otherMatrix) const;		// Dot Product
 	void operator=(const Matrix& otherMatrix);						
-	double& operator[](uint16_t index) const;						      // Access element in Matrix[index]
+	double& operator[](uint16_t index) const;			// Access element in Matrix[index]
 	double& operator() (uint16_t row, uint16_t col) const;		// Access element in Matrix in (row, col) form (y, x) starting at (0,0) for first element
 
 private:
